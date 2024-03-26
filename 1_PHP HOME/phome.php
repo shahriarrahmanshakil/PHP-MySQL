e1c44e1e8243c9e39fb7f463dcda98c4d54f11ae
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-03-26 06:38:57 +0600

    php home initialize and php folder is upgraded

---
+++ 1_PHP HOME/phome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php home</title>
+</head>
+<body>
+    <h1>PHP HOME</h1>
+    <?php
+        echo "PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.<br>";
+        echo "PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP."
+    ?>
+</body>
+</html>
