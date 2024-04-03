2692a902154bd1b4ed2abb7eb8b5bfd15c3c27ba
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-03 06:26:07 +0600

    php data types added

---
+++ 8_PHP Data Types/datatype.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Type</title>
+</head>
+<body>
+    <h1>PHP Data Types</h1>
+    <?php
+    $number = 3450;
+    var_dump($number);
+
+    $text = "Hello Bangladesh";
+    var_dump($text);
+    ?>
+</body>
+</html>
---
+++ 8_PHP Data Types/string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String</title>
+</head>
+<body>
+    <h2>PHP String</h2>
+    <?php
+    $text = "Learning PHP";
+    var_dump($text);
+    echo "<br>";
+    echo "<br>";
+    $text2 = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, assumenda maxime cum praesentium commodi iusto, a voluptas veritatis, dignissimos nobis aperiam enim eos quidem nisi maiores placeat nulla molestias earum!";
+    var_dump($text2);
+    ?>
+</body>
+</html>
