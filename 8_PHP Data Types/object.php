6a0231d771eb9f005e8e444c3052894b3eeaec1a
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-04 10:30:55 +0600

    object is added;

--- 8_PHP Data Types/object.php
+++ 8_PHP Data Types/object.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Object</title>
 </head>
 <body>
+    <?php
+        class Car{
+            public $color;
+            public $model;
+            public $engine;
+
+            public function __construct($color,$model,$engine){
+                    $this->color  = $color;
+                    $this->model  = $model;
+                    $this->engine = $engine;
+            }
+            public function message(){
+                return "My Car is Aweosome this is $this->color.''.$this->model.''.$this->engine.''.is supper fast";
+            }
+
+        }
+        $mycar = new Car("Powder Blue","Suzuki rx-200","vxc567");
+        echo $mycar;
+
     
+    ?>
 </body>
 </html>
