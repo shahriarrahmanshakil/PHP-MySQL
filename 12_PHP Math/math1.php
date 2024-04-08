e1b4cbb1aea162d03f06f8ca8c35447b46ad76e6
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-08 15:12:21 +0600

    php math folder is init

---
+++ 12_PHP Math/math1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php math</title>
+</head>
+<body>
+    <h3>PHP Math</h3>
+    <?php
+        echo "php pi() Function " ;
+
+        echo pi(); #php pi() Function 
+    ?>
+</body>
+</html>
