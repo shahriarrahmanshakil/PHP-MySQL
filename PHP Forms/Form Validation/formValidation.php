8c2f9b3b73b5a31aa0835f0929998b2f0d8a8306
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-22 19:12:24 +0600

    php forms updated

--- PHP Forms/Form Validation/formValidation.php
+++ PHP Forms/Form Validation/formValidation.php
@@ -7,6 +7,22 @@
 </head>
 <body>
     <h2>PHP Form Validation Example</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <label for="name">Name</label><br>
+        <input type="text" id="name" name="name" size="30"><br><br>
+        <label for="email">Email</label><br>
+        <input type="email" id="email" name="email" size="30"><br><br>
+        <label for="website">WEbsite</label><br>
+        <input type="text" id="website" name="website" size="30"><br><br>
+        <label for="comment">Comment</label><br>
+        <textarea name="comment" id="comment" cols="30" rows="5" placeholder="comment here"></textarea><br><br>
+        <label for="gender"> Gender : </label>
+        <input type="radio" id="male" name="gender">
+        <label for="male">Male</label>
+        <input type="radio" id="female" name="gender">
+        <label for="male">Female</label><br><br>
+        <input style="width: 245px;;" type="submit" name="btnsubmit" value="submit">
+    </form>
     
 </body>
 </html>
