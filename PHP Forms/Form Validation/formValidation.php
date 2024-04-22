dae9d0574d1d8eb2e4797c338f2e7f1af26ef74b
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-22 12:40:39 +0600

    php basic folder is created and added others folder

---
+++ PHP Forms/Form Validation/formValidation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation</title>
+</head>
+<body>
+    <h2>PHP Form Validation Example</h2>
+    
+</body>
+</html>
