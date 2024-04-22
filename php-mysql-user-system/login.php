b56e60704f8463d8ce7c21c6885b03e21c601509
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-22 07:02:26 +0600

    php-mysql-user-system Upgrade

--- php-mysql-user-system/login.php
+++ php-mysql-user-system/login.php
@@ -1,29 +1,41 @@
 <?php
-    session_start();
-    $connectwithDB = mysqli_connect('localhost','root','','user_system');
+    session_start(); #session start here
+
+    $connectwithDB = mysqli_connect('localhost','root','','user_system'); #create connection with database
     
-    $emptyemailmessage = $emptypasswordmassage = '';
-    if(isset($_POST['btnlogin'])){
+    $emptyemailmessage = $emptypasswordmassage = $loginfailedmassage = ''; #error massage initilaize(empty variable set) 
+
+    if(isset($_POST['btnlogin']))
+    {
         $logemail    = $_POST['log_email'];
         $logpassword = $_POST['log_password'];
         
-        $md5_logpassword = md5($logpassword);
+        $md5_password = md5($logpassword); #passwor Encryption function used
 
-        if(empty($logemail)){
-            $emptyemailmessage = "Input you'r Valid Email";
+        if(empty($logemail))
+        {
+            $emptyemailmessage = "Input you'r Valid Email"; #error massage set here
         }
 
-        if(empty($logpassword)){
-            $emptypasswordmassage = "Input you'r Valid Password";
+        if(empty($logpassword))
+        {
+            $emptypasswordmassage = "Input you'r Valid Password"; #error massage set here
         }
 
-        if(!empty($logemail) && !empty($logpassword)){
+        if(!empty($logemail) && !empty($logpassword))
+        {
             $selectdata = "SELECT * FROM signup_form 
-                           WHERE email = '$logemail' AND password = '$md5_logpassword' ";
-                           
-            if(mysqli_query($connectwithDB,$selectdata) == TRUE ){
-                $_SESSION['login'] = 'login success';
+                           WHERE email = '$logemail' AND password = '$md5_password' "; #query_lnguage_used for select data from database 
+            $querydata  = mysqli_query($connectwithDB,$selectdata);
+
+            $arraydata  = mysqli_fetch_assoc($querydata); #query_data converted to array
+
+            if( $arraydata == TRUE)
+            {
+                $_SESSION['login'] = 'login success'; #session variable set
                 header('location:dashboard.php');
+            }else{
+                $loginfailedmassage = 'Please Enter valid Email and Password'; #error massage set here
             }
         }
     }
@@ -31,36 +43,43 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <link rel="stylesheet" href="login.css">
-</head>
-<body>
-    <div style="display: flex; justify-content:space-between;">
-        <div style="margin-left: 60px;">
-            <img src="login.jpg" alt="Login form img" width="500px" height="500px">
-        </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login Form</title>
+        <link rel="stylesheet" href="login.css"> <!--css file added-->
+    </head>
+    
+    <body>
+        <div style="display: flex; justify-content:space-between;">
 
-        <div style="margin-top:120px; margin-right:200px;">
-            <?php if(isset($_GET['UserCreated'])){echo "User Created Succesfully" ; }  ?>
-            <form action="login.php" method="post">
+            <div style="margin-left: 60px;">
+                <img src="login.jpg" alt="Login form img" width="500px" height="500px">
+            </div>
+
+            <div style="margin-top:120px; margin-right:200px;">
+                <form action="login.php" method="post">
                     <label for="log_email">Email</label><br>
-                    <input type="email" id="log_email" name="log_email" value="<?php if(isset($_POST['btnlogin'])){echo $logemail;}?>"> <br>
-                    <?php if(isset($_POST['btnlogin'])){echo "<span style = 'color:red;'> $emptyemailmessage </span>";}?><br><br>
+                    <input type="email" id="log_email" name="log_email"> <br>
+                    <?php if(isset($_POST['btnlogin'])){echo "<span style = 'color:red;'> $emptyemailmessage </span>";}?><br><br>   <!--error massage used here -->
                     
                     <label for="log_password">Password</label><br>
-                    <input type="password" id="log_password" name="log_password"  value="<?php if(isset($_POST['btnlogin'])){echo $logpassword;}?>"><br>
-                    <?php if(isset($_POST['btnlogin'])){echo "<span style = 'color:red;'> $emptypasswordmassage </span>";}?><br><br>
+                    <input type="password" id="log_password" name="log_password"> <br>
+                    
+                    <?php if(isset($_POST['btnlogin'])){echo "<span style = 'color:red;'> $emptypasswordmassage </span>";}?>        <!--error massage used here -->
+                    <?php if(isset($_POST['btnlogin'])){echo "<span style = 'color:red;'> $loginfailedmassage </span>";}?>          <!--error massage used here -->
+                    <?php if(isset($_GET['UserCreated'])){echo "<span style ='color:green;'> User Created Succesfully </span>";} ?> <!--error massage used here -->
+                    
+                    <br><br>
+                    
                     <input type="submit" name="btnlogin" value="Login">
                     <p>
                         Not have an Account?
                         <button><a href='signup.php' target="_blank">Sign up</a></button>
                     </p>  
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-   
-</body>
+    
+    </body>
 </html>
