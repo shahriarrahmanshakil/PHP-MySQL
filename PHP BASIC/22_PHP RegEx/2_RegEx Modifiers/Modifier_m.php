7f625884f5ba474544ce26f7176d6bc8e0efe6aa
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-04-24 09:53:02 +0600

    forms validation is updated

--- PHP BASIC/22_PHP RegEx/2_RegEx Modifiers/Modifier_m.php
+++ PHP BASIC/22_PHP RegEx/2_RegEx Modifiers/Modifier_m.php
@@ -9,12 +9,20 @@
     <h1>Regex Modifier: m</h1>
     <p>How many times does the phrase "you" occur at the beginning of a line in the text:</p>
     <pre>Avoid heat stroke amid heatwave:
-    DGHS issues eight directives</pre>
+        Avoid DGHS issues eight directives</pre>
     <?php
-        $text = "Avoid heat stroke amid heatwave heat is DGHS issues eight directives";
-        $pattern = "/^stroke/m";
+        $text = "Avoid heat stroke amid heatwave heat is DGHS\nAvoid issues eight directives";
+        $pattern = "/^Avoid/m";
         echo preg_match_all($pattern, $text);
     ?>
+
+    <p>The matches were found here:</p>
+    <?php
+        echo preg_replace($pattern,'*',$text);
+    ?>
+
+    <br><br>
+    <p>Each match was replaced by a (*) character</p>
 
 </body>
 </html>
---
+++ PHP Forms/Forms Required/FormsRequired.php
@@ -0,0 +1,84 @@
+<?php
+    #Create variables and set to empty values
+    $nameerror = $emailerror = $website_error  = $gendererror = "";
+    $name = $email = $website = $comment = $gender = "";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+         if(empty($_POST['name'])){
+            $nameerror = "Name is Required";
+         }else{
+            $name = $_POST["name"];
+         }
+
+         if(empty($_POST['email'])){
+            $emailerror = "Email is Required";
+         }else{
+            $email = $_POST["email"];
+         }
+        
+         if(empty($_POST['website'])){
+            $website = "";
+         }else{
+            $website = $_POST["website"];
+         }
+
+         if(empty($_POST['comment'])){
+            $comment = "";
+         }else{
+            $comment = $_POST["comment"];
+         }
+
+         if(empty($_POST['gender'])){
+            $gendererror = "Select you'r gender";
+         }else{
+            $gender = $_POST["gender"];
+         }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation</title>
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h2>PHP Form Validation Example</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <label for="name">Name</label>
+        <span style="color: red;">*</span><br>
+        <input type="text" id="name" name="name" size="30">
+        <span class ='error'><?php echo $nameerror; ?></span><br><br>
+
+        <label for="email">Email</label>
+        <span style="color: red;">*</span><br>
+        <input type="email" id="email" name="email" size="30">
+        <span class ='error'><?php echo $emailerror; ?></span><br><br>
+
+        <label for="website">Website</label><br>
+        <input type="text" id="website" name="website" size="30"><br><br>
+
+        <label for="comment">Comment</label><br>
+        <textarea name="comment" id="comment" cols="30" rows="5" placeholder="comment here"></textarea><br><br>
+
+        <label for="gender">Gender :</label>
+        <span style="color: red;">*</span>
+        <input type="radio" id="male" name="gender">
+        <label for="male">Male</label>
+        <input type="radio" id="female" name="gender">
+        <label for="male">Female</label> &nbsp;&nbsp;&nbsp;&nbsp;
+        <span class ='error'><?php echo $gendererror; ?></span><br><br>
+
+        <input style="width: 245px;;" type="submit" name="btnsubmit" value="submit">
+        
+    </form>
+    
+</body>
+</html>
