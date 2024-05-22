42be0f5e43c73afa438adc4872f33ff0573643db
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-22 15:03:20 +0600

    PHP-File-Handling-added

---
+++ 3_PHP Advanced/3_PHP File Handling/fileHandling.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File Handling</title>
+</head>
+<body>
+    <?php
+        echo readfile("file.txt");
+    ?>
+</body>
+</html>
