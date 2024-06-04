489b296a6e3af569329214142321b6520e3d0f2e
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-05 02:02:58 +0600

    minimumvalue.php added

--- PHP-Problem-Solution/Problem-01/MinimumValue.php
+++ PHP-Problem-Solution/Problem-01/MinimumValue.php
@@ -1,6 +1,19 @@
 <?php
+    #problem - 01
+    echo "Problem 01 <br>";
+    echo "<br>";
+    
     $value1 = array(10,20,-7,30);
-    $minvalue = min($value1);
-    echo $minvalue; 
+    $minvalue1 = abs(min($value1));
+    echo "Minimum Absolute value = " . $minvalue1;
+
+    echo "<br>";
+
+    $value2 = array(11,22,33,44,55,66,-66,-33,-32);
+    $minvalue2 = abs(min($value2));
+    echo "Minimum Absolute value = " . $minvalue2;
+
+
+    
 
 ?>
