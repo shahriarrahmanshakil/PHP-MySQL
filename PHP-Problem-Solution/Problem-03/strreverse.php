f4fc9ef1427185ec4ff16c2f24b2883936777728
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-05 13:45:04 +0600

    problem-3 added

---
+++ PHP-Problem-Solution/Problem-03/strreverse.php
@@ -0,0 +1,23 @@
+<?php
+/* Problem 3:
+Given a sentence, keep the order of the words same, but reverse the characters of each word.
+For example, if the given sentence is: ‘I love programming’
+The result should be: ‘I evol gnimmargorp’
+Here the order of the words is the same as the given sentence, but the order of the characters in the
+words is reversed. */
+
+$text = 'I love programming';
+$reversetxt = strrev($text);
+echo $reversetxt;
+
+
+
+
+
+
+
+
+
+
+
+?>
