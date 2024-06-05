6f70786f62dffd7e14d6a5757ecac8a2268942b8
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-05 11:06:30 +0600

    Problem-02 added

---
+++ PHP-Problem-Solution/Problem-02/countWords.php
@@ -0,0 +1,17 @@
+<?php
+    /* Problem 2:
+    Given a few paragraphs in a file, read the file and count how many words are there.
+    For example, if there is a file with the following contents:
+    Nunc ex lorem, ullamcorper ut eleifend ac, pellentesque non dolor.
+    You need to output: 10
+    Because there are 10 words.
+    Note: There can be multiple paragraphs. You need to count all of the words from all of the
+    paragraphs.
+    You need to read the data from a file. */
+
+    //slove
+    $myfile = readfile(countWords.txt);
+    echo $myfile;
+
+
+?>
