1041505f5d36beda3983c275db7246751c8dfe77
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-12 23:57:09 +0600

    methods are added

--- 4_PHP OOP/PHP OOP/index.php
+++ 4_PHP OOP/PHP OOP/index.php
@@ -2,11 +2,30 @@
     class fruits {
         public $name;
         public $color;
+        public $weight;
+
+        function set_name($name,$color,$weight){
+            $this->name = $name;
+            $this->color = $color;
+            $this->weight = $weight;
+        }
+
+        function get_name(){
+            return  $this->name.$this->color.$this->weight;
+        }
     }
     $apple = new fruits();
-    $apple->name = "Apple";
-    $apple->color = "Red";
-    echo $apple;
+    $apple->set_name("Apple ","Red ","120gm");
+    echo $apple->get_name();
+    $apple->name = "Mango";
+    $apple->color = "Yellow";
+    $apple->weight = "100";
+
+    // echo $apple->name."<br>";
+    // echo $apple->color."<br>";
+    // echo $apple->weight;
+
+  
 
 
 
