a5d4550a2762b5ad6f60cbad910e0a3fa4e0764a
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-13 11:38:09 +0600

    constructor method added

--- 4_PHP OOP/2_PHP OOP - Constructor/person.php
+++ 4_PHP OOP/2_PHP OOP - Constructor/person.php
@@ -13,11 +13,43 @@
     $person1->age = 27;
     $person1->sex = "Male";
     $person1->show();
+
     echo "<br>";
     echo "<br>";
+
     $person2 = new person();
     $person2->name = "Rafia Kabir Khan";
     $person2->age = 29;
     $person2->sex = "Female";
     $person2->show();
+?>
+
+<?php echo "<br>"; ?>
+<?php echo "<br>"; ?>
+<?php echo "<h1><u>Using Constructor Method</u></h1>"; ?>
+<?php echo "<br>"; ?>
+
+<?php
+//added constructor method
+    class anotherPerson {
+        public $name;
+        public $address;
+
+        function __construct($nam="abc",$add='xyz'){
+            $this->name = $nam;
+            $this->address = $add;
+        }
+
+        function shows(){
+            echo "Name : ".$this->name."<br>"."Address : ".$this->address;
+        }
+    }
+    $person4 = new anotherPerson('Rakibul Islam','Malibag,Dhaka');
+    $person4->shows();
+
+    echo "<br>";
+    echo "<br>";
+
+    $person5 = new anotherPerson('Raihan Molla Raju','Lalbag,Dhaka');
+    $person5->shows();
 ?>
