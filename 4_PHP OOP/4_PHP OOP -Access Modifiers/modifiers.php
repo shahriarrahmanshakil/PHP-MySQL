614d47e3a4e0663c7c9faf8be88b29ed56c2a7f4
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-14 18:48:30 +0600

    access modifiers protected and private added

--- 4_PHP OOP/4_PHP OOP -Access Modifiers/modifiers.php
+++ 4_PHP OOP/4_PHP OOP -Access Modifiers/modifiers.php
@@ -2,7 +2,11 @@
 //Access Modifiers
 class furniture{
     public $name;
+    protected $quality;
+    private $color;
 }
 $chair = new furniture();
 echo $chair->name = "My Chair";
+$chair->$quality; // gives an error because protected
+$chair->color; // gives an error because private
 ?>
