77f1be9e614bfd2f158e8234166aef5817113827
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-16 08:36:23 +0600

    php opp __constructor method add

--- 4_PHP OOP/2_PHP OOP - Constructor/fruits.php
+++ 4_PHP OOP/2_PHP OOP - Constructor/fruits.php
@@ -25,28 +25,25 @@
 // $b->name = "Rahman";
 // echo $b->name;
 
-
-
-
-
 ?>
+
 
 <?php
 
 class fruit{
-  public $name;
-  public $color;
-  public function __construct($name,$color){
-    $this->name = $name;
-    $this->color = $color;
-  }
-  public function intro(){
-    echo "the fruit is".$this->name."and the color is".$this->color;
-  }
+      public $name;
+      public $color;
 
-}
- $fruit =new fruit("APPLE","RED");
- $fruit->intro();
+      public function __construct($name,$color){
+        $this->name = $name;
+        $this->color = $color;
+      }
+      public function intro(){
+        echo "the fruit is".$this->name."and the color is".$this->color;
+      }
+    }
 
+    $fruit =new fruit("APPLE","RED");
+    $fruit->intro();
 
 ?>
