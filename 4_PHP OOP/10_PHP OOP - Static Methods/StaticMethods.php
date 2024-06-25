9be51be27edadf586185bbe30246dcf5da0f1b10
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-25 12:13:42 +0600

    php opp added more class in static methods

--- 4_PHP OOP/10_PHP OOP - Static Methods/StaticMethods.php
+++ 4_PHP OOP/10_PHP OOP - Static Methods/StaticMethods.php
@@ -1,9 +1,44 @@
 <?php
     class greeting{
-        public function wellcome(){
+        public static function wellcome(){
             echo "Hello php";
         }
     }
-    $message = new greeting();
-    echo $message->wellcome();
+    // $message = new greeting();
+    // echo $message->wellcome();
+    greeting::wellcome();
+    echo "<br>";
+    ////////////////////////////////////////////////////////////////////
+
+    class message{
+        public static function hello(){
+            echo "This is Hello Function";
+        }
+
+        public function __construct(){
+            self::hello();
+        }
+    }
+    new message();
+    echo "<br>";
+
+    //////////////////////////////////////////////////////////////////////////
+
+    class A{
+        public static function a(){
+            echo "this is function A";
+        }
+    }
+
+    class B{
+        public function b(){
+            echo A::a();
+        }
+    }
+
+    $B = new B();
+    echo $B->b();
+    echo "<br>";
+
+    ////////////////////////////////////////////////////////////////////////////////
 ?>
