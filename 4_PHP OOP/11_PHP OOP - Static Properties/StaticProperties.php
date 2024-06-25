27e5fad7f82a0aefdc5027de3cb9e4a52308268f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-25 14:05:01 +0600

    php oop static properties added

--- 4_PHP OOP/11_PHP OOP - Static Properties/StaticProperties.php
+++ 4_PHP OOP/11_PHP OOP - Static Properties/StaticProperties.php
@@ -17,6 +17,26 @@
     // echo flower::$rose;
     $myrose = new flower();
     echo $myrose->myrose();
+    echo "<br>";
+    /////////////////////////////////////////////////////////////////
+
+    class man{
+        public static $name = "This is My Name";
+        public static $age = "My Age ***";
+
+        public static function purpose(){
+            return self::$name .'<br>'. self::$age;
+        }
+    }
+
+    class women extends man{
+        public function pur(){
+            return parent::purpose();
+        }
+    }
+    $m = new women();
+    echo $m->pur();
+    
 
 
 
