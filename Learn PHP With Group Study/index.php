1c54c6bbe8f1832703522507c7497a3ca9392984
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-02 23:36:24 +0600

    Learn PHP With Group Study

---
+++ Learn PHP With Group Study/index.php
@@ -0,0 +1,38 @@
+<?php
+echo "Hello World \n";
+echo "Hello World \n";
+
+//variable 
+$letter = "abc \n";
+echo $letter;
+
+$name = "Bangladesh";
+$age = 20;
+
+$word = "age";
+
+echo "The Number is {$age},{$name}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
