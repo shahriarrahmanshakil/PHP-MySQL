ee152692fbc72120eeb2957612d0e81529a06acf
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-12 17:34:24 +0600

    variable output file added

---
+++ 1_PHP BASIC/6_PHP Variables/output.php
@@ -0,0 +1,26 @@
+<?php 
+$txt = "Bangladesh";
+echo "I Love $txt .";
+echo "<br>";
+
+///////////////////////////////
+
+$test1 = "PHP";
+$test2 = "Hypertext Pre-processor";
+echo "We Love ". $test1 . " ". $test2;
+
+///////////////////////////////////////
+echo "\n";
+echo "<br>";
+
+$x = 10;
+$y = 20;
+$z = $x + $y;
+echo "Total : {$z}";
+
+
+
+
+
+
+?>
