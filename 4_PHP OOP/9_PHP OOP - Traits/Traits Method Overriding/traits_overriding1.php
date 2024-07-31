38b88f102a925e8acfe07c23c147ef9106e03761
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-31 13:21:36 +0600

    trait overriding method added

---
+++ 4_PHP OOP/9_PHP OOP - Traits/Traits Method Overriding/traits_overriding1.php
@@ -0,0 +1,26 @@
+<?php
+    trait test{
+        public function hello(){
+            echo "Hello from Trait";
+        }
+    }
+
+    class A{
+        public function hello(){
+            echo "Hello from class A";
+        }
+    }
+
+    class B extends A{
+        use test;
+
+        // public function hello()
+        // {
+        //     echo "Hello from class B";
+        // }
+    }
+
+    $b1 = new B();
+    $b1->hello();
+
+?>
