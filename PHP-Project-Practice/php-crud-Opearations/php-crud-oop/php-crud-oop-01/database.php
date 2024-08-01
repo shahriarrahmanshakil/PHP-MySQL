65cd4efe1cceb53bb38184e596432bf886870ae3
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-01 17:05:41 +0600

    delete sql queries added

--- PHP-Project-Practice/php-crud-Opearations/php-crud-oop/php-crud-oop-01/database.php
+++ PHP-Project-Practice/php-crud-Opearations/php-crud-oop/php-crud-oop-01/database.php
@@ -27,12 +27,24 @@
             $this->connect_db->query($insert_sql);
         } 
 
-        public function update(){
+        //update_data
+        public function update($e_n,$e_m,$e_id){
+            $update_sql = "UPDATE employee_information 
+                           SET employee_name = '$e_n', employee_email = '$e_m' 
+                           WHERE employee_id = '$e_id'";
+            $this->connect_db->query($update_sql);
+        }
 
+        //delete_data
+        public function delete(){
+            $delete_sql = "DELETE FROM employee_information WHERE employee_id = '4'";
+            $this->connect_db->query($delete_sql);
         }
     }
 
     $db = new database("localhost","root","","php_oop_crud_operations");
-    $db->insert();
+    // $db->insert();
+    // $db->update('MD Tuhin Mia','tuhin@gmail.com','4');
+    $db->delete();
 
 ?>
