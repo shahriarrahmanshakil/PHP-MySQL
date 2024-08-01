3af6302a12317a96e27ad0d8842266dc822ddc3f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-01 23:35:33 +0600

    Search Engine in PHP MySQL added

---
+++ Search Engine in PHP MySQL/data.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Information</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <input type="text" id="search" name="search" placeholder="Search Here">
+        <input type="submit" name="btnsearch" value="Search">
+    </form>
+</body>
+</html>
