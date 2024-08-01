e3acbbf6b4af4a4590b9e7a3dcf3bdb53e881138
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-01 16:48:44 +0600

    insert data added

--- PHP-Project-Practice/php-crud-Opearations/php-crud-oop/php-crud-oop-01/database.php
+++ PHP-Project-Practice/php-crud-Opearations/php-crud-oop/php-crud-oop-01/database.php
@@ -4,6 +4,7 @@
         public $userName;
         public $passWord;
         public $dbName;
+        public $connect_db;
 
         public function __construct($hostName,$userName,$passWord,$dbName)
         {
@@ -12,14 +13,26 @@
             $this->passWord = $passWord;
             $this->dbName = $dbName;
 
-            $connect_db = new mysqli($this->hostName = $hostName, $this->userName = $userName, $this->passWord = $passWord, $this->dbName = $dbName);
+            $this->connect_db = new mysqli($this->hostName = $hostName, $this->userName = $userName, $this->passWord = $passWord, $this->dbName = $dbName);
 
-            if($connect_db->connect_error){
-                die("Connection Failed : $connect_db->connect_error");
+            if($this->connect_db->connect_error){
+                die("Connection Failed : $this->connect_db->connect_error");
             }
+        }
+        //insert_data
+        public function insert(){
+            $insert_sql = "INSERT INTO employee_information(employee_name,employee_email) 
+                    VALUES('Shahriar Rahman Shakil', 'shahriar@gmail.com')";
+
+            $this->connect_db->query($insert_sql);
+        } 
+
+        public function update(){
+
         }
     }
 
     $db = new database("localhost","root","","php_oop_crud_operations");
+    $db->insert();
 
 ?>
